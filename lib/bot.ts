de6a9e552c3d1a4aeeebe0e85697be50d6cf7d38
Christos Panagiotakopoulos <chrispanag@gmail.com>
2019-10-02 17:31:26 +0300

    add secret.ts

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,17 +1,14 @@
 import { Bot } from 'ebony-framework';
 import { MessengerAdapter, MessengerUser } from 'ebony-messenger-adapter';
 
+import { fbConfig, mongodbUri } from './secret';
+
 import getStarted from './modules/getStarted';
 
-export const adapter = new MessengerAdapter({
-    pageId: process.env.FB_PAGE_ID ? process.env.FB_PAGE_ID : "",
-    pageToken: process.env.FB_PAGE_TOKEN ? process.env.FB_PAGE_TOKEN : "",
-    appSecret: process.env.FB_APP_SECRET ? process.env.FB_APP_SECRET : "",
-    webhookKey: "123"
-});
+export const adapter = new MessengerAdapter(fbConfig);
 
 export const bot = new Bot<MessengerUser>([adapter], {
-    mongodbUri: process.env.MONGODB_URI as string
+    mongodbUri
 });
 
 // This should get imported at the end
